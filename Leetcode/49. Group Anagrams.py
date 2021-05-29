4f80e41288db01447e4fbdf189d2f8918dbe65a8
kdw <76869915+kimdw92@users.noreply.github.com>
2021-05-29 11:29:32 +0900

    Update 49. Group Anagrams.py

--- Leetcode/49. Group Anagrams.py
+++ Leetcode/49. Group Anagrams.py
@@ -1,7 +1,7 @@
 # 05 그룹 애너그램
         # 애너그램 판단은 정렬해서 하면 쉬움
         # 정렬된 애너그램을 dafault key로 사용해서 dict에 추가
-        # 문자 정렬은 sorted만 사용 가능
+        # 문자 정렬은 sorted() 가능, 리스트로 바뀌기 때문에 ''.joint()으로 다시 문자열 
         # anagrams.values() -> dict의 값만 리턴
         # ''.join() : str -> list
 
