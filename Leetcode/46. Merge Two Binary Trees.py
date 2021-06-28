ed1854946205adcca6eabf607059e2c9512a0eba
kdw <76869915+kimdw92@users.noreply.github.com>
2021-06-29 00:17:37 +0900

    Create 46. Merge Two Binary Trees.py

---
+++ Leetcode/46. Merge Two Binary Trees.py
@@ -0,0 +1,12 @@
+# 트리 617 두 이진 트리 병합
+# 이진트리
+# 풀이: 재귀 탐색
+class Solution:
+    def mergeTrees(self, root1: TreeNode, root2: TreeNode) -> TreeNode:
+        if root1 and root2:
+            node = TreeNode(root1.val + root2.val)
+            node.left = self.mergeTrees(root1.left, root2.left)
+            node.right = self.mergeTrees(root1.right, root2.right)
+            return node
+        else:
+            return root1 or root2
