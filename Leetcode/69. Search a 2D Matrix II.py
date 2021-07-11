0c21c50c302bc8cdf7ec66315b9f82ef232c2908
kdw <76869915+kimdw92@users.noreply.github.com>
2021-07-12 01:12:37 +0900

    Create 69. Search a 2D Matrix II.py

---
+++ Leetcode/69. Search a 2D Matrix II.py
@@ -0,0 +1,16 @@
+# 240 2D 행렬 검색2
+
+# 풀이1:
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        answer = False
+        m, n = 0, len(matrix[0])-1
+        
+        while m >= 0 and m < len(matrix) and n >= 0 and n < len(matrix[0]):
+            if matrix[m][n] < target:
+                m += 1
+            elif matrix[m][n] > target:
+                n -= 1
+            else:
+                return True
+        return answer
