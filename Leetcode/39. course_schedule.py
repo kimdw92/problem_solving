205d75b3399ece5078fcb0c5e2744075dee53293
darius <76869915+kimdw92@users.noreply.github.com>
2021-08-17 02:10:32 +0900

    Update 39. course_schedule.py

--- Leetcode/39. course_schedule.py
+++ Leetcode/39. course_schedule.py
@@ -31,3 +31,44 @@ class Solution:
                 return False
             
         return True
+    
+    
+# 다른 풀이
+# path 대신에 set() 이용
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = collections.defaultdict(list)
+        for x, y in prerequisites:
+            graph[x].append(y)
+            
+        # 이미 방문한 곳
+        traced = set()
+        # 탐색이 끝난 후
+        visited = set()
+        
+        
+        def dfs(i):
+            # 순환 구조이면 False
+            if i in traced:
+                return False
+            # 이미 방문한 노드이면 True
+            if i in visited:
+                return True
+            
+            traced.add(i)
+            for y in graph[i]:
+                if not dfs(y):
+                    return False
+                
+            # 탐색 종료 후 순환 노드 탐색
+            traced.remove(i)
+            # 탐색 종료 후 방문 노드 추가
+            visited.add(i)
+            
+            return True
+        
+        for x in list(graph):
+            if not dfs(x):
+                return False
+            
+        return True
