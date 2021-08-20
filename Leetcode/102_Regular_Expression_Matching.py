5785690b0bb9d62d3179f91ac3ebbad6384d0338
darius <76869915+kimdw92@users.noreply.github.com>
2021-08-20 11:28:03 +0900

    Update 102_Regular_Expression_Matching.py

--- Leetcode/102_Regular_Expression_Matching.py
+++ Leetcode/102_Regular_Expression_Matching.py
@@ -1,4 +1,7 @@
-# 재귀를 이용한 풀이
+# 10. Regular Expression Matching
+# Hard
+
+# 재귀를 이용한 풀이 1352 ms	14.2 MB
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         if not p: return not s
@@ -16,3 +19,39 @@ class Solution:
         else:
             # 그냥 다음 문자 비교
             return first_match and self.isMatch(s[1:], p[1:])
+        
+# DP, 하향식 36 ms	14.6 MB
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        memo = {}
+        def dp(i, j):
+            if (i, j) not in memo:
+                if j == len(p): # 패턴이 None이면
+                    ans = (i == len(s)) # 텍스트도 None이어야 True
+                else:
+                    first_match = i < len(s) and p[j] in {s[i], '.'}
+                    if j+1 < len(p) and p[j+1] == '*':
+                        ans = dp(i, j+2) or (first_match and dp(i+1, j))
+                    else:
+                        ans = first_match and dp(i+1, j+1)
+                        
+                memo[i, j] = ans
+            return memo[i, j]
+    
+        return dp(0, 0)
+
+# DP, 상향식
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        dp = [[False] * (len(p) + 1) for _ in range(len(s) + 1)]
+        
+        dp[-1][-1] = True
+        
+        for i in range(len(s), -1, -1):
+            for j in range(len(p) - 1, -1, -1):
+                first_match = i < len(s) and p[j] in {s[i], '.'}
+                if j+1 < len(p) and p[j+1] == '*':
+                    dp[i][j] = dp[i][j+2] or first_match and dp[i+1][j]
+                else:
+                    dp[i][j] = first_match and dp[i+1][j+1]
+        return dp[0][0]
