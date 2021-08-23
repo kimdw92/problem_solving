9c8d39205024b603d276fe656064e0359c5a0049
darius <76869915+kimdw92@users.noreply.github.com>
2021-08-23 13:32:11 +0900

    Update 106_Next Permutation.py

--- Leetcode/106_Next Permutation.py
+++ Leetcode/106_Next Permutation.py
@@ -1,33 +1,27 @@
 # 31 Next Permutation
 # Medium
 # 투포인터, 조금 트리키한 문제... 솔루션을 봐야함
-# 구현했는데 아직 안돌아감
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
+        if len(nums) < 2:
+            return nums
+        
         left, right = len(nums)-2, len(nums)-1
-        while left >= 0 and nums[left] >= nums[right]:
+        while nums[left] >= nums[right]:
             if left == 0:
                 break
             left -= 1
             right -= 1
           
         while right < len(nums) and nums[left] < nums[right]:
-            right += 1    
-            
+            right += 1                
         right -= 1
-        print(left, right)
+        
         if nums[left] < nums[right]:
             nums[left], nums[right] = nums[right], nums[left]
-            print(nums)
-            nums = nums[:left+1] + sorted(nums[left+1:])
-            print(nums)
+            nums[left+1:] = sorted(nums[left+1:])
         else:
             nums.sort()
-            
-        print(nums)
             
             
             
