594e01c8e4ab6e3178112e38488e54b738ecb63a
darius <76869915+kimdw92@users.noreply.github.com>
2021-09-25 19:16:06 +0900

    Create 34_kakaointern2021_숫자문자열과영단어.py

---
+++ programmers/34_kakaointern2021_숫자문자열과영단어.py
@@ -0,0 +1,18 @@
+# level 1
+# replace() 함수
+
+def solution(s):
+    dict = {'zero' : '0',
+           'one' : '1',
+           'two' : '2',
+           'three' : '3',
+           'four' : '4',
+           'five' : '5',
+           'six' : '6',
+           'seven' : '7',
+           'eight' : '8',
+           'nine' : '9'}
+    answer = s
+    for key, value in dict.items():
+        answer = answer.replace(key, value)
+    return int(answer)
